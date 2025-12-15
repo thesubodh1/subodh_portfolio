eeeb12276b7dc1e05d26fd9106b0143ef8b04c23
thesubodh1 <thesubodh1@gmail.com>
2025-12-15 19:27:43 +0545

    repo name added into base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/subodh_portfolio/',
   plugins: [react()],
 })
